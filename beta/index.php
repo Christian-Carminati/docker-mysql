6e0280168d6acd6cc8138d9ad660bad55cf3b3c6
Christian-Carminati <03chriscar@gmail.com>
2022-02-16 15:49:54 +0000

    update

--- beta/index.php
+++ beta/index.php
@@ -1,3 +1,19 @@
 <?php
-    echo "beta";
+$servername = "localhost";
+$username = "root";
+$password = "test";
+
+// Create connection
+try {
+  $db = new PDO('mysql:host=db;dbname=localhost', 'root', 'test');
+} catch (PDOException $e) {
+  print "Error!: " . $e->getMessage() . "<br/>";
+  die();
+}
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
